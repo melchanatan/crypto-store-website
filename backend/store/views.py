3ec95d57f09368abf42c4b86425e7e577bffe073
mels <chanatan.melmel@gmail.com>
2024-04-19 19:41:18 +0700

    feat: implemented update_promotion endpoint

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,13 +12,43 @@ class UserCartViewSet(viewsets.ModelViewSet):
     queryset = UserCart.objects.all()
     serializer_class = UserCartSerializer
     
-    def partial_update(self, request, pk=None):
+    @action(methods=['PATCH'], detail=True)
+    def update_promotion(self, request, pk=None):
         """
-        Update a user's cart.
+        Update promotion for user cart.
         """
-        
+    
         user_id = pk
+        request_data = request.data
         
+        promotion_id = request_data.get('promotion_id')
+        if promotion_id == None:
+            return Response({'error': 'promotion_id is required'}, status=400)
+                
+        # Handle user remove promotion using "-" as parameter.
+        user_cart = get_object_or_404(UserCart, user_id=user_id)
+        if (promotion_id == "-"):
+            user_cart.selected_promotion = None
+            user_cart.save()
+        
+            return Response({
+                'status': 'success',
+                'message': 'Promotion removed successfully',
+                'user_cart': UserCartSerializer(user_cart).data
+            })
+        else:
+            # Add Promotion to user cart
+            
+            # Get user cart and promotion object
+            promotion_object = get_object_or_404(Promotion, pk=promotion_id)
+            
+            user_cart.selected_promotion = promotion_object
+            user_cart.save()
+            
+            return Response({
+                'user_cart': UserCartSerializer(user_cart).data,
+            })
+
 
     def retrieve(self, request, pk=None):
         """        
@@ -48,8 +78,8 @@ class ProductViewSet(viewsets.ModelViewSet):
 
 class CartItemViewSet(viewsets.ModelViewSet):
     queryset = CartItem.objects.all()
-    serializer_class = CartItemSerializer
-
+    serializer_class = ItemInCartSerializer
+    
 
 class PromotionViewSet(viewsets.ModelViewSet):
     queryset = Promotion.objects.all()
