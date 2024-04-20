9ff5ac0d2c8211bedd5a13b0e7fd554da736b555
mels <chanatan.melmel@gmail.com>
2024-04-21 02:31:34 +0700

    feat: styling discount code function on Cart.jsx

--- frontend/src/components/global/Cart.jsx
+++ frontend/src/components/global/Cart.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { IoCartOutline } from "react-icons/io5";
 import { FaPlus } from "react-icons/fa6";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 const Cart = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -19,12 +20,28 @@ const Cart = () => {
                 >
                     <CartItem />
                     <CartItem />
-                    <div className="flex items-end justify-between relative px-3 pt-4 border-t-[1px] solid border-gray-400 shadow-[0px_-3px_5px_0px_#00000024]">
-                        <p className="leading-4">
-                            total:
-                            <h4 className="font-bold text-xl mb-1t">$100</h4>
-                        </p>
+                    <div className="flex flex-col gap-2 relative px-3 pt-4 border-t-[1px] solid border-gray-400 shadow-[0px_-3px_5px_0px_#00000024]">
+                        <div className="flex items-end justify-between">
+                            <p>
+                                total:
+                            </p>
+                            <h4 className="font-bold text-xl flex gap-2 items-center">
+                                <span className="px-2 py-1 bg-green-400 rounded-full text-white font-prompt text-sm">-40%</span>
+
+                                <div className="relative">
+                                    <span className="h-[3px] bg-black w-[120%] absolute translate-y-1/2 bottom-1/2 rotate-[-10deg] left-0"></span>
+                                    100
+                                </div>
+                                <FaLongArrowAltRight /> 80$
+
+                            </h4>
+                        </div>
+                        <div className="flex">
+                            <input type="text" placeholder="discount code" className="w-full bg-gray-200 text-center " />
+                            <button className="bg-primary text-white px-3 py-1 font-prompt">apply</button>
+                        </div>
                         <button className="bg-primary text-white px-3 py-2 font-prompt">checkout</button>
+
                     </div>
                 </div >
             </>
@@ -34,6 +51,7 @@ const Cart = () => {
     const CartItem = () => {
 
         return (
+
             <div className="flex items-center justify-between gap-2 font-prompt py-3 px-3">
                 <img src="/default-avatar.jpeg" alt="product img" className="w-[32px] h-[32px] object-cover rounded-full" />
                 <label>
